f19c38038a871bea6869c2b0f9813d21bf0a4f29
Travis Schriner <C15Travis.Schriner@usafa.edu>
2013-11-03 20:07:44 -0700

    added device driver files and rearranged my driver header file

--- Interupts.c
+++ Interupts.c
@@ -10,4 +10,38 @@
  *  	best game since PONG
  */
 
-#include <Interupts.h>
+#include "Interupts.h"
+#include "LCD/lcd.h"
+
+unsigned char initPlayer()
+{
+        return 0x80;
+}
+
+void printPlayer(unsigned char player)
+{
+        writeCommandByte(player);
+        writeDataByte('*');
+}
+
+void clearPlayer(unsigned char player)
+{
+        writeCommandByte(player);
+        writeDataByte(' ');
+}
+
+unsigned char movePlayer(unsigned char player, unsigned char direction)
+{
+        switch (direction) {
+                //
+                // update player position based on direction of movement
+                //
+        }
+
+        return player;
+}
+
+char didPlayerWin(unsigned char player)
+{
+        return player == 0xC7;
+}
